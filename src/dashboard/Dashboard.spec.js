1eb544453a5a31cd15a164875afe52accf285eae
Will Schulz <will.schulz29@gmail.com>
2019-04-17 20:29:05 -0400

    some changes

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard.js';
+
+describe('<Dashboard />', () => {
+    it('matches snapshot', () => {
+        const domTree = renderer.create(<Dashboard />).toJSON();
+
+        expect(domTree).toMatchSnapshot();
+    })
+})
